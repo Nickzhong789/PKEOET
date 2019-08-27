19b55e3c249da75fa2c4e8723285b5298f70a38a
nickzhong789 <nickzhong789@sina.com>
2019-08-28 02:50:11 +0800

    fix client and server

--- client.py
+++ client.py
@@ -1,52 +1,93 @@
 import socket
 import sys
+import json
 
 from pkeoet import PKEOET
+from pypbc import *
 
 
-def IVgen(num):
-    stored_params = """type a
-    q 8780710799663312522437781984754049815806883199414208211028653399266475630880222957078625179422662221423155858769582317459277713367317481324925129998224791
-    h 12016012264891146079388821366740534204802954401251311822919615131047207289359704531102844802183906537786776
-    r 730750818665451621361119245571504901405976559617
-    exp2 159
-    exp1 107
-    sign1 1
-    sign0 1
-    """
+def list_to_str(l):
+    s = ''
+    for e in l:
+        s += str(e) + '|'
+    
+    return s[:-1]
 
-    pkeoet = PKEOET(stored_params, num)
 
-    pkeoet.setup()
+def str_to_c(file, pairing):
+    with open(file, 'r') as f:
+        ct = f.read().splitlines()
 
-    m_a = pkeoet.init()
+    seg = len(ct) // 2
 
-    pkeoet.dkg()
+    c = []
+    for ci in ct:
+        ci_list = ci.split('|')
 
-    sk1, pk1 = pkeoet.ukg()
-    tk1 = pkeoet.tkg(sk1)
+        ci1 = Element(pairing, G1, value=ci_list[0])
+        ci2 = Element(pairing, G1, value=ci_list[1])
+        ci3 = Element(pairing, GT, value=ci_list[2])
+        ci4 = Element(pairing, GT, value=ci_list[3])
 
-    sk2, pk2 = pkeoet.ukg()
-    tk2 = pkeoet.tkg(sk2)
+        c.append([ci1, ci2, ci3, ci4])
 
-    c1 = pkeoet.enc(pk1, tk1, sk1, m_a)
-    c2 = pkeoet.enc(pk2, tk2, sk2, m_a)
+    c1 = c[:seg]
+    c2 = c[seg:]
+
+    return c1, c2
+
+
+def IVgen(num, json_file):
+    with open(json_file, 'r') as f:
+        server_info = json.load(f)
+
+    stored_params = server_info['sp']
+    params = Parameters(param_string=stored_params)
+    pairing = Pairing(params)
+
+    pkeoet = PKEOET(pairing, num)
+    # print(pairing == pkeoet.pairing)
+
+    pk1 = [Element(pairing, G1, value=pki) for pki in server_info['user1']['pk'].split('|')]
+    tk1 = [Element(pairing, G2, value=tki) for tki in server_info['user1']['tk'].split('|')]
+
+    pk2 = [Element(pairing, G1, value=pki) for pki in server_info['user2']['pk'].split('|')]
+    tk2 = [Element(pairing, G2, value=tki) for tki in server_info['user2']['tk'].split('|')]
+
+    c_file = './ciphertext.txt'
+    c1, c2 = str_to_c(c_file, pairing)
+
+    dpk = Element(pairing, G2, value=server_info['dpk'])
+    g = Element(pairing, G1, value=server_info['g'])
+    h = Element(pairing, G1, value=server_info['h'])
 
     v = pkeoet.pTest(c1, c2, pk1, pk2, tk1, tk2)
 
     return v
 
-v = IVgen(10)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+if __name__ == "__main__":
+    json_file = './cli.json'
+    v = IVgen(3, json_file)
+    print(v[0])
 
-host = socket.gethostname() 
-port = 8001
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
-s.connect((host, port))
+    # host = socket.gethostname() 
+    # port = 8001
 
-msg = "hhhhhhhhhhh\r\n"
-s.send(msg.encode('utf-8'))
-s.close()
+    # s.connect((host, port))
 
-print (msg)
+    with open('./v.txt', 'w') as f:
+        msg = ''
+        for vi in v:
+            m = list_to_str(vi)
+            f.write(m)
+            f.write('\n')
+            msg += m + '.'
+    
+    
+    # s.send(msg[:-1].encode('utf-8'))
+    # print (msg)
+    
+    # s.close()
--- server.py
+++ server.py
@@ -1,23 +1,71 @@
 import socket
 import sys
+from pypbc import *
+import json
 
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# host = socket.gethostname()
-host = "172.18.92.12"
-port = 8001
-
-server_socket.bind((host, port))
-server_socket.listen(5)
-
-while True:
-    client_socket, addr = server_socket.accept()
-
-    print("Addr is: %s" % str(addr))
+def msg_to_v(m, file):
+    with open(file, 'r') as f:
+        d_info = json.load(f)
     
-    msg=client_socket.recv(10240)
+    sp = d_info['sp']
+    params = Parameters(param_string=sp)
+    pairing = Pairing(params)
 
-    print(msg.decode('utf-8'))
+    dsk_s = d_info['dsk']
+    dsk_v = int(dsk_s, 16)
+    dsk = Element(pairing, Zr, value=dsk_v)
+
+    v = []
+    vt = m.split('.')
+    for vti in vt:
+        v_s = vti.split('|')
+        vi = [Element(pairing, GT, value=v_s[i]) for i in range(len(v_s))]
+        v.append(vi)
     
-    client_socket.close()
+    return v, dsk
+
+
+def dTest(tv, dsk):
+    if((tv[2]**dsk) != tv[3]):
+        return None
+    else:
+        if((tv[0]**dsk) != tv[1]):
+            return 0
+        else:
+            return 1
+
+if __name__ == "__main__":
+    json_file = './discriminator.json'
+
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    host = socket.gethostname()
+    # host = "172.18.92.12"
+    port = 8001
+
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+
+    print("Server Start...............................")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+
+        print("Addr is: %s" % str(addr))
+
+        msg = client_socket.recv(30720)
+
+        # print(msg.decode('utf-8'))
+
+        d_list = []
+        v, dsk = msg_to_v(msg.decode('utf-8'), json_file)
+
+        print(v)
+        for vi in v:
+            d_list.append(dTest(vi, dsk))
+
+        print(d_list)
+
+        client_socket.close()
